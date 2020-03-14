Fixed Scaling to Inf/NaN

ae.py
@@ -96,8 +96,8 @@ class AutoEncoder(nn.Module):
         loss = F.mse_loss(sampled, result, reduction="none")
 
         # Dim Batch*Prior
-        # loss = torch.mean(loss, dim=(1, 2, 3))
-        loss = loss.sum(dim=(1, 2, 3))
+        loss = torch.mean(loss, dim=(1, 2, 3))
+        # loss = loss.sum(dim=(1, 2, 3))
 
         # Dim Batch,Priors
         loss = loss.view(*gridShape[:2])

data/config.py
@@ -1046,6 +1046,10 @@ unsupervised_config = yolact_plus_resnet50_config.copy(
         "sampling_grid": [128, 128],
         "use_maskiou": False,
         "iou_gauss_dim": [50, 50],
+        # Wrap coordinates at 2
+        "gauss_wrap": 1,
+        # for Gaussian
+        "max_scale": 1.5,
         "max_num_detections": 30,
         # "gauss_iou_samples": 40,
         "max_size": 300,
@@ -1054,7 +1058,6 @@ unsupervised_config = yolact_plus_resnet50_config.copy(
         "mask_proto_prototype_activation": torch.tanh,
         "mask_proto_coeff_activation": torch.tanh,
         # "preserve_aspect_ratio": True,
-        "loc_scale":1/10
     }
 )
 

layers/modules/unsupervised_loss.py
@@ -11,6 +11,10 @@ import pdb
 from torchvision.utils import make_grid
 
 from torch.utils.tensorboard import SummaryWriter
+import torchsnooper
+import snoop
+
+torchsnooper.register_snoop()
 
 
 def unsup_writer(Writer):
@@ -71,8 +75,12 @@ class UnsupervisedLoss(nn.Module):
 
             # IoU not included because not needed by variance
             out = {
-                "loc": twoIndexThree(all_results["loc"], keep),
-                "mask": twoIndexThree(all_results["mask"], keep),
+                "loc": all_results["loc"][
+                    torch.arange(all_results["loc"].shape[0]).unsqueeze(-1), keep
+                ],
+                "mask": all_results["mask"][
+                    torch.arange(all_results["mask"].shape[0]).unsqueeze(-1), keep
+                ],
                 "conf": torch.gather(all_results["conf"], 1, keep),
                 "proto": proto_data,
             }
@@ -80,6 +88,7 @@ class UnsupervisedLoss(nn.Module):
             losses["variance_loss"] = self.variance(
                 original, out["loc"], out["mask"], out["conf"], out["proto"]
             )
+            print(losses)
             out["losses"] = losses
 
         return out
@@ -102,9 +111,9 @@ class UnsupervisedLoss(nn.Module):
             conf, top_k_conf, dim=-1, largest=True
         )
         # loc with size [batch,num_priors, 4], or 5 for unsup
-        sorted_loc = twoIndexThree(loc, sorted_conf_idx)
+        sorted_loc = loc[torch.arange(loc.shape[0]).unsqueeze(-1), sorted_conf_idx]
         # masks shape: Shape: [num_priors, mask_dim]
-        sorted_mask = twoIndexThree(mask, sorted_conf_idx)
+        sorted_mask = mask[torch.arange(mask.shape[0]).unsqueeze(-1), sorted_conf_idx]
 
         # Dim: Batch, Detections, i,j
         gauss = gaussian.unnormalGaussian(maskShape=cfg.iou_gauss_dim, loc=sorted_loc)
@@ -124,8 +133,9 @@ class UnsupervisedLoss(nn.Module):
         gauss_intersection = torch.sum(torch.min(gauss_grid, dim=3)[0], dim=[3, 4])
         gauss_union = torch.sum(torch.max(gauss_grid, dim=3)[0], dim=[3, 4])
 
+        # Positive required to not make NaN
         # Batch, Detections, Detections
-        gauss_iou = gauss_intersection / (gauss_union)
+        gauss_iou = gauss_intersection / (gauss_union + cfg.positive)
 
         # Batch, Detections
         iou_max, _ = gauss_iou.triu(diagonal=1).max(dim=1)
@@ -150,9 +160,14 @@ class UnsupervisedLoss(nn.Module):
         # Remove Lower Triangle and Diagonal
         final_scale = iou * conf_matrix.triu(1)
 
+        try:
+            loc = loc[torch.arange(loc.shape[0]).unsqueeze(-1), keep]
+        except Exception as e:
+            print(e)
+            __import__("pdb").set_trace()
         # Dim batch,priors
         # try:
-        ae_loss = self.autoencoder(original, twoIndexThree(loc, keep))
+        ae_loss = self.autoencoder(original, loc)
         # except RuntimeError:
         # pdb.set_trace()
         # ae_loss but in scores
@@ -165,7 +180,7 @@ class UnsupervisedLoss(nn.Module):
         ae_grid = ae_grid * final_scale
 
         # Divide by Priors
-        return torch.mean(ae_grid)
+        return torch.sum(ae_grid)
 
 
 class MyException(Exception):

utils/gaussian.py
@@ -5,6 +5,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.multivariate_normal import _batch_mahalanobis
 from data.config import cfg, mask_type
+import torchsnooper
+import snoop
+
+torchsnooper.register_snoop()
 
 
 def lincomb(predictions=None, proto=None, masks=None):
@@ -20,50 +24,46 @@ def lincomb(predictions=None, proto=None, masks=None):
     return torch.einsum("acde,abe->abcd", proto, masks)
 
 
+def rotation_matrix(theta):
+    # Theta shape: torch.shape(batch,num_priors)
+    sin, cos = torch.sin(theta), torch.cos(theta)
+
+    # shape: torch.shape(batch,num_priors,2)
+    rot_0 = torch.stack((cos, -sin), dim=2)
+    rot_1 = torch.stack((sin, cos), dim=2)
+
+    # shape: torch.shape(batch,num_priors,2,2)
+    rot = torch.stack((rot_0, rot_1), dim=2)
+    return rot
+
+
+# @snoop
 def gauss_loc(loc):
     # Σ is equal to RSS(R-1)
     # Cholesky=T=RS
     # calculate this instead
-    # loc shape: torch.size(batch_size,num_priors,6)
+    # loc shape: torch.size(batch_size,num_priors,5)
     locShape = list(loc.shape)
 
-    # NOTE: without tanh, something becomes NaN
-    loc = torch.tanh(loc)
     mean = loc[:, :, :2]
-    cov = loc[:, :, 2:].view(*locShape[:2], 2, 2)
-    cov = (cov.permute(0, 1, 3, 2)) @ cov
-    # cov = cov + torch.eye(2) * cfg.positive
-    # NOTE: this is where the diagonal line comes from
-    cov = torch.where(
-        # Where the diagonal is negative
-        torch.diag_embed(torch.diagonal(cov, dim1=-1, dim2=-2)) < 0,
-        torch.abs(cov),
-        cov,
-    )
-    try:
-        cholesky = torch.cholesky(cov.float())
-    except RuntimeError:
-        print("Singular CUDA")
-        cholesky = torch.cholesky(cov.float() + torch.eye(2))
-    # NOTE: removed loc_scale
-    cholesky = cholesky
-    if torch.isnan(cholesky).any():
-        print("Not Symmetric Positive Semi-definite")
-        __import__("pdb").set_trace()
+    # mean = torch.tanh(mean)
+    mean = torch.fmod(mean, cfg.gauss_wrap)
 
-        # @rayandrews
-        # this is inplace operation
-        # will result error in backprop!
-        # cholesky[torch.isnan(cholesky)] = 0
+    theta = loc[:, :, 2]
+    rot = rotation_matrix(theta)
 
-        # below is the safe version
-        cholesky = torch.where(
-            torch.isnan(cholesky), torch.zeros_like(cholesky), cholesky
-        )
-
+    scale_coeff = loc[:, :, 3:5]
+    # Positive required
+    scale_coeff = (
+        (torch.sign(scale_coeff) * cfg.max_scale)
+        - torch.fmod(scale_coeff, cfg.max_scale)
+    ) ** 2 + cfg.positive
+    scale = torch.diag_embed(scale_coeff)
+    cholesky = rot @ scale
     return mean, cholesky
 
 
+# @snoop
 def mahalanobisGrid(predictions=None, maskShape=None, loc=None):
     if maskShape is None:
         maskShape = list(predictions["proto"].shape)[1:3]

yolact.py
@@ -128,7 +128,7 @@ class PredictionModule(nn.Module):
 
             if cfg.gaussian:
                 self.gauss_layer = nn.Conv2d(
-                    out_channels, self.num_priors * 6, **cfg.head_layer_params
+                    out_channels, self.num_priors * 5, **cfg.head_layer_params
                 )
             else:
                 self.bbox_layer = nn.Conv2d(
@@ -244,7 +244,7 @@ class PredictionModule(nn.Module):
                 src.gauss_layer(gauss_x)
                 .permute(0, 2, 3, 1)
                 .contiguous()
-                .view(x.size(0), -1, 6)
+                .view(x.size(0), -1, 5)
             )
         else:
             bbox_x = src.bbox_extra(x)